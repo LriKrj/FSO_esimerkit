772cbe9b07c250c8fe46784dbc16125f3f16df35
LriKrj <lauri.karjalainen3@hotmail.com>
2023-12-15 12:41:46 +0200

    muutos

--- nodetesti/index.js
+++ nodetesti/index.js
@@ -23,7 +23,14 @@ let notes = [
   }
 ]
 
-
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger)
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
