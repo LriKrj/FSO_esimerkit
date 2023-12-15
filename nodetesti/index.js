e9d50d48625ff036ed16c97ba95cf4bbd652b14d
LriKrj <lauri.karjalainen3@hotmail.com>
2023-12-15 14:05:26 +0200

    full

--- nodetesti/index.js
+++ nodetesti/index.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
+
 let notes = [
 
 
@@ -23,14 +25,8 @@ let notes = [
   }
 ]
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-app.use(requestLogger)
+
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
